ba731eeb12918a763e96157e3b53ed836509d239
席舟 <568353394@qq.com>
2025-05-28 11:34:41 +0800

    fix: 打包插件

--- src/main/jdk11/module-info.java
+++ src/main/jdk11/module-info.java
@@ -9,8 +9,10 @@ module cn.xz.study.proxy {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
     requires org.apache.commons.io;
-    requires com.pagoda.platform.ssh;
+    //requires com.pagoda.platform.ssh;
     requires java.base;
+    //requires com.jfoenix.controls;
+    //requires com.jcraft.jsch;
     // 暴露包 sample 给 javafx 的模块们，使其可以在运行时使用反射访问
     opens cn.xz.study.proxy to javafx.graphics, javafx.fxml;
     opens cn.xz.study.proxy.controller to javafx.graphics, javafx.fxml;
